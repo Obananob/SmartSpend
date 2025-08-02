dbd15fbda59b5c7786ca19de9a490abbfe6b380e
Bankole Ridwan <ridwan.bankole@kenkeputa.com>
2025-08-02 11:24:10 -0500

    Use 6 features

--- app.py
+++ app.py
@@ -17,42 +17,81 @@ page = st.sidebar.radio("Choose a page", ['Dashboard', 'About Us', 'Meet the Tea
 
 if page == "Dashboard":
     st.title("SmartSpent Deployment")
+    st.write("Play with the demo app")
 
-    # upload the file
-    data = st.file_uploader("Upload you transaction data", type=['csv'])
+    input_method = st.radio("Chose your method to input the data", ["Upload file CSV", 'Manual Entry'])
 
-    if data is not None:
-        try:
-            # Read the CSV file into a Pandas DataFrame
-            df = pd.read_csv(data)
-            df = df.dropna().reset_index(drop=True)
-            st.write("Top 5 rows")
-            st.write(df.head())
-            
-
-        except Exception as e:
-            st.error(f"Error processing file: {e}")
-    else:
-        st.info("Please upload a CSV file to proceed.")
-
-
-
-
-    # get the customer info and the preprocess it
-    start = st.text_input("Starting point")
     
-    if start:
-        start = int(start)
-    end = st.text_input("Ending point")
-    if end:
-        end = int(end)
 
+    if input_method == "Upload file CSV": 
+        # upload the file
+        upload_data = st.file_uploader("Upload you transaction data", type=['csv'])
+
+        if upload_data is not None:
+            try:
+                # Read the CSV file into a Pandas DataFrame
+                df = pd.read_csv(upload_data)
+                df = df.dropna().reset_index(drop=True)
+                st.write("Top 5 rows")
+                st.write(df.head())
+
+                # get the customer info and the preprocess it
+                start = st.text_input("Starting point")
+                
+                if start:
+                    start = int(start)
+                end = st.text_input("Ending point")
+                if end:
+                    end = int(end)
+
+
+                cust_info = df.iloc[start:end, :]
+
+                
+
+            except Exception as e:
+                st.error(f"Error processing file: {e}")
+        else:
+            st.info("Please upload a CSV file to proceed.")
+    elif input_method == 'Manual Entry':
+        data = []
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:        
+            CUST_ID = st.text_input("Customer ID")
+            BALANCE = st.text_input("BALANCE")
+        
+        with col2: 
+            BALANCE_FREQUENCY = st.text_input("BALANCE_FREQUENCY")
+            PURCHASES = st.text_input("PURCHASES")
+        
+        with col3:
+            ONEOFF_PURCHASES = st.text_input("ONEOFF_PURCHASES")
+            INSTALLMENTS_PURCHASES = st.text_input("INSTALLMENTS_PURCHASES")
+        
+        if all([CUST_ID, BALANCE]):
+            data.append({"CUST_ID": CUST_ID,
+                        "BALANCE": float(BALANCE),
+                        "BALANCE_FREQUENCY": float(BALANCE_FREQUENCY),
+                        "PURCHASES": PURCHASES,
+                        "ONEOFF_PURCHASES": float(ONEOFF_PURCHASES),
+                        "INSTALLMENTS_PURCHASES": float(INSTALLMENTS_PURCHASES)
+                        })
+
+        df = pd.DataFrame(data)
+
+        cust_info = df.copy()
+
+    else:
+        st.write("Enter your data")
 
 
 
     if st.button("Get Prediction"):
        
-        cust_info = df.iloc[start:end, :]
+        
+        
         X = cust_info.drop(columns='CUST_ID')
         scaled_cust_info = scaler.transform(X)
         reduced_cust_info = pca.transform(scaled_cust_info)
